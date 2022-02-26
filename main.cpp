8f1539f2ecfc81d3b91bc8373e3b4fb04acfe2a3
Aleksey Bubencov <bubencov0110@mail.ru>
2022-02-26 23:11:16 +0700

    ...

--- main.cpp
+++ main.cpp
@@ -6,13 +6,17 @@
 #include <list>
 	
 	//#pragma pack(push, 1)
-	typedef struct _Block{
+	/*
+	typedef struct _BlockHead{
 		bool isFreeBlock;			//если false, то next и prev соседние блоки
 		struct _Block *prev;
 		struct _Block *next;			
 		size_t size;
 		uint8_t mem[1];
-	} Block;
+	} BlockHead;
+	*/
+	
+	
 	//#pragma pack(pop)
 
 	size_t blockMetadataSize(){
@@ -23,14 +27,18 @@
 	const size_t BLOCKMETADATASIZE = blockMetadataSize();
 	//#define BLOCKMETADATASIZE (sizeof(Block) - 1)
 	#define BLOCKSIZE(block) (block->size + BLOCKMETADATASIZE)
-	Block *head;
-	void *mem;
-	size_t memSize;
+	Block *head = 0;
+	void *mem = NULL;
+	size_t memSize = 0;
 	
 	
 	std::list<Block *> blocks;
 	
+	
+	
+	
 	Block *createBlock(bool isFreeBlock, void *buf, size_t blockSize){
+	    
 		//printf("createBlock:\n");
 		//printf("buf = %p\n", buf);
 		//printf("blockSize = %d\n", blockSize);
@@ -64,7 +72,29 @@
 		return block;
 	}
 	
-	 // Эта функция будет вызвана перед тем как вызывать myalloc и myfree
+	void deleteBlock(Block *block){
+
+		printf("deleteBlock\n");
+
+		for(auto i = blocks.begin(); i != blocks.end(); ++i){
+			if(*i == block) blocks.erase(i);
+		}
+		
+		if(isFreeBlock(block)){
+			removeFromList(block);
+		} else {
+			Block *rightBlock = getRightBlock(block);
+			Block *leftBlock = getLeftBlock(block);
+			if(isFreeBlock(rightBlock)){
+				expand(block, leftBlock);
+			}
+			if(isFreeBlock(leftBlock)){
+				block = expand(block, leftBlock);
+			}
+			freeMem(block);
+		}
+	}
+	// Эта функция будет вызвана перед тем как вызывать myalloc и myfree
     // используйте ее чтобы инициализировать ваш аллокатор перед началом
     // работы.
     //
@@ -75,15 +105,15 @@
     
 	void mysetup(void *buf, std::size_t size)
 	{
-		head = NULL;
+		//head = NULL;
 		printf("mysetup:\n");
 		//printf("buf = %p\n", buf);
 		//printf("size = %d\n", size);
 		//printf("head = %p\n", head);
-		mem = buf;
-		memSize = size;
+		//mem = buf;
+		//memSize = size;
 		createBlock(true, buf, size);
-		Block *block = (Block *)buf;
+		//Block *block = (Block *)buf;
 		//printf("BLOCKSIZE(block) = %d\n", BLOCKSIZE(block));
 		//printf("head = %p\n", head);
     }
@@ -100,51 +130,42 @@
 		//*freeBlock;
 		Block *allocBlock;
 		size_t allocBlockSize = size + BLOCKMETADATASIZE;
+		
+		Block *block = findFreeBlock(size);
+		
+		if(block == NULL) return NULL;
+		
+		allocMem(block);
+		/*
 		do{
 			
 			if(freeBlock->size >= allocBlockSize){
 				freeBlock->size -= allocBlockSize;
 				void *buf = freeBlock->mem + freeBlock->size;
 				allocBlock = createBlock(false, buf, allocBlockSize);
-				allocBlock->prev = freeBlock;
-				uint8_t *memEnd = (uint8_t *)mem + memSize - 1;
-				uint8_t *allocBlockEnd = allocBlock->mem + allocBlock->size - 1;
-				if(allocBlockEnd < memEnd){
-					allocBlock->next = (Block *)(allocBlock->mem + allocBlock->size);
-					Block *rightBlock = allocBlock->next;
-					if(!rightBlock->isFreeBlock)
-						rightBlock->prev = allocBlock;
+				//allocBlock->prev = freeBlock;
+				//uint8_t *memEnd = (uint8_t *)mem + memSize - 1;
+				//uint8_t *allocBlockEnd = allocBlock->mem + allocBlock->size - 1;
+				//if(allocBlockEnd < memEnd){
+					//allocBlock->next = (Block *)(allocBlock->mem + allocBlock->size);
+					//Block *rightBlock = allocBlock->next;
+					//if(!rightBlock->isFreeBlock)
+						//rightBlock->prev = allocBlock;
 				}
 				//->size + BLOCKMETADATASIZE;
 				return allocBlock->mem;
 			}
 			freeBlock = freeBlock->next;
 		} while(freeBlock != head);
-		return NULL;
+		return NULL;*/
 	}
 
-	void deleteBlock(Block *block){
-		printf("deleteBlock\n");
-		for(auto i = blocks.begin(); i != blocks.end(); ++i){
-			if(*i == block) blocks.erase(i);
-		}
-		if(block->isFreeBlock){
-			if(block == head){
-				if(head->next == head) 
-					return;
-				head = head->next;
-			}
-			block->next->prev = block->prev;
-			block->prev->next = block->next;
-		} else {
-			createBlock(true, block, BLOCKSIZE(block));
-		}
-	}
+	
     // Функция освобождения
 	void myfree(void *p)
 	{
 	    printf("myfree\n");
-		Block *block = (Block *)((uint8_t *)p - BLOCKMETADATASIZE);
+		//Block *block = (Block *)((uint8_t *)p - BLOCKMETADATASIZE);
 		auto it = blocks.begin();
 		int n = blocks.size();
 		for(int i = 0; i < n; i++, it++){
@@ -152,7 +173,7 @@
 				blocks.erase(it);
 			}
 		}
-		printf("block = %p\n", block);
+		printf("block = %p\n", block);/*
 		Block *leftBlock = block->prev;
 		Block *rightBlock = block->next;
 		if(rightBlock != NULL) 
@@ -168,8 +189,8 @@
 				return;
 			} else {
 				leftBlock->next = block->next;
-			}
-		deleteBlock(block);
+			}*/
+		deleteBlock(getBlock(p));
 	}
 ////////////////////////////////////////////
 
